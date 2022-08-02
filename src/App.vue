408235c257b899250bd3e01f03f1d24ba38431a1
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-08-02 18:43:29 +0200

    add jobs view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <nav class="flex justify-center">
-    <router-link class="mx-2" exact-active-class="text-green-500" to="/">Home</router-link> |
-    <router-link class="mx-2" exact-active-class="text-green-500" :to="{name:'About'}">About</router-link> |
-    <router-link class="mx-2" exact-active-class="text-green-500" :to="{name:'Jobs'}">Jobs</router-link>
+    <router-link class="mx-2" exact-active-class="text-green-500" to="/"
+      >Home</router-link
+    >
+    |
+    <router-link
+      class="mx-2"
+      exact-active-class="text-green-500"
+      :to="{ name: 'About' }"
+      >About</router-link
+    >
+    |
+    <router-link
+      class="mx-2"
+      exact-active-class="text-green-500"
+      :to="{ name: 'Jobs' }"
+      >Jobs</router-link
+    >
   </nav>
-  <router-view />
+  
+  <ViewWrapper>
+    <router-view />
+  </ViewWrapper>
 </template>
+
+<script>
+</script>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,10 +1,7 @@
 <template>
-  <ViewWrapper>
-    <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id">
-      <h2>{{ job.title }}</h2>
-    </div>
-  </ViewWrapper>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
 </template>
 
 <script>
