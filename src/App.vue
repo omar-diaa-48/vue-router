33ceea57cb86422135a22e893efd9ff9cb8d4690
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-08-02 18:26:30 +0200

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="flex justify-center">
+    <router-link exact-active-class="text-green-500" to="/">Home</router-link> |
+    <router-link exact-active-class="text-green-500" :to="{name:'About'}">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
